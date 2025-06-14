f0cf5bc6ecec1d0f6103c625aa36df3e23f69940
José Hernández <jh265626@gmail.com>
2025-06-14 15:12:54 -0500

    Add images of fanpage cdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,16 @@ import react from "@astrojs/react";
 import aiRobotsTxt from "astro-ai-robots-txt";
 import min from "astro-min";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), icon(), react(), aiRobotsTxt(), min()],
+
   vite: {
     plugins: [svgr()],
   },
+
+  adapter: netlify(),
 });
