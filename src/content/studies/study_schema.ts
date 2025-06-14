ab8d8fb5d5188294292a1cbcc0c95c98c67b5bd3
José Hernández <jh265626@gmail.com>
2025-06-14 17:07:02 -0500

    Add projects: ciname-java and tienda-lacteos

--- src/content/studies/study_schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "astro:content";
-export const study_schema = z.object({
-  Title: z.string(),
-  Duration: z.string(),
-  Where: z.string(),
-  Image_path: z.string(),
-  Position: z.number(),
-});
--- src/content/work_experience/work_experience_schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "astro:content";
-
-export const work_experience_schema = z.object({
-  Role: z.string(),
-  Where: z.string(),
-  Duration: z.string(),
-  Description: z.string(),
-  Functions: z.array(z.string()),
-});
