5046033d7f75161d49158267cee4a28ce2c17cab
José Hernández <jh265626@gmail.com>
2025-07-03 13:03:49 -0500

    add transitions

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -21,25 +21,30 @@ export default function ProjectCard({ slug, data, index }) {
       className="project--card"
       aria-label={`Ir a la página del proyecto: ${data.Title}`}
     >
-      <div className="project--card-title ">
+      <div className="project--card-title">
         <strong transition:name={`project-title-${data.Title}`}>
           {data.Title}
         </strong>
       </div>
-      <picture>
-        <source srcSet={data.Cover_path_full.src} media="(min-width: 1200px)" />
-        <source srcSet={data.Cover_path_xl.src} media="(min-width: 1024px)" />
-        <source srcSet={data.Cover_path_md.src} media="(min-width: 500px)" />
-        <img
-          src={data.Cover_path.src}
-          alt={data.Title}
-          loading={index == 0 ? "eager" : "lazy"}
-          decoding="async"
-          width={index == 0 ? "620" : "600"}
-          className="project--card-image"
-          transition:name={`project-${data.Title}`}
-        />
-      </picture>
+      <div className="project--card-image-div">
+        <picture>
+          <source
+            srcSet={data.Cover_path_full.src}
+            media="(min-width: 1200px)"
+          />
+          <source srcSet={data.Cover_path_xl.src} media="(min-width: 1024px)" />
+          <source srcSet={data.Cover_path_md.src} media="(min-width: 500px)" />
+          <img
+            src={data.Cover_path.src}
+            alt={data.Title}
+            loading={index == 0 ? "eager" : "lazy"}
+            decoding="async"
+            width={index == 0 ? "620" : "600"}
+            className="project--card-image"
+            transition:name={`project-${data.Title}`}
+          />
+        </picture>
+      </div>
 
       <div className="project--card-description">{data.Short_description}</div>
       <div
